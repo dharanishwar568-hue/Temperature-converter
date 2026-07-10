5fcf67270740aba22a08652ee352b344ca18a088
dharanishwar568-hue <dharanishwar568@gmail.com>
2026-07-10 14:53:25 +0530

    Add files via upload

---
+++ temperature converter.py
@@ -0,0 +1,12 @@
+celsius=float(input("enter your celsius:"))
+
+farenheit=(celsius*9/5)+32
+
+print("Farenheit temperature=",farenheit)
+
+
+farenheit=float(input("enter your farenheit:"))
+
+celsius=(farenheit-32)*5/9
+
+print("Celsius temperature=",celsius)
